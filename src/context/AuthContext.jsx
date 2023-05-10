126d7c6c7cd37c97e9f5feb37b1f39b666c27995
bruno <truongtritin.bee@gmail.com>
2023-05-10 23:42:14 +0700

    part for Login, still have bug

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,65 @@
+import { createContext, useReducer, useEffect } from "react";
+import apiService from "../app/apiService";
+import { isValidToken } from "../utils/jwt"; //check the token is still working?
+import React from "react";
+
+const initialState = {
+  isInitialized: false, //check is the app render ?
+  isAuthenicated: false, //check is the  user have login?
+  user: null, //check the data of the user
+};
+
+const AuthContext = createContext({ ...initialState });
+
+const setSession = (accessToken) => {
+  if (accessToken) {
+    window.localStorage.setItem("access Token", accessToken);
+    apiService.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+  } else {
+    window.localStorage.removeItem("access token");
+    delete apiService.defaults.headers.common.Authorization;
+  }
+};
+
+const INITIALIZE = "AUTH.INITIALIZE";
+const LOGIN_SUCCESS = "AUTH.LOGIN_SUCCESS";
+const REGISTER_SUCCESS = "AUTH.REGISTER_SUCCESS";
+const LOGOUT = "AUTH.LOGOUT";
+const UPDATE_PROFILE = "AUTH.UPDATE_PROFILE";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isAuthenicated: true,
+        user: action.payload.user,
+      };
+    default:
+      return state;
+  }
+};
+
+const AuthProvider = ({ children }) => {
+  const [state, dispacth] = useReducer(reducer, initialState);
+
+  const login = async ({ email, password }, callback) => {
+    const response = await apiService.post("/auth/login", { email, password });
+    const { user, accessToken } = response.data;
+
+    setSession(accessToken);
+    dispacth({
+      type: LOGIN_SUCCESS,
+      payload: { user },
+    });
+    callback();
+  };
+
+  return (
+    <AuthContext.Provider value={{ ...state, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider, AuthContext };
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import { FCheckBox, FormProvider, FTextField } from "../components/form";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import { useLocation, useNavigate, Link as RouterLink } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import {
+  Alert,
+  Container,
+  IconButton,
+  InputAdornment,
+  Link,
+  Stack,
+} from "@mui/material";
+import { Visibility } from "@mui/icons-material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid Email").required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
+
+const defaultValues = {
+  email: "",
+  password: "",
+  remember: true,
+};
 
 const LoginPage = () => {
+  //
+  const auth = useAuth();
+  //
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
+  //
+  const {
+    handleSubmit,
+    reset,
+    setError,
+    formState: { error, isSubmitting },
+  } = methods;
+  //
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [showPassword, setShowPassword] = useState();
+  //
+
+  const onSubmit = async (data) => {
+    const from = location.state?.from?.pathname || "/";
+    let { email, password } = data;
+    try {
+      await auth.login({ email, password }, () => {
+        navigate(from, { replace: true });
+      });
+    } catch (error) {
+      reset();
+      setError("responseError", error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <Container>
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={3}>
+          {error.responseError && (
+            <Alert severity="error">{error.responseError.message}</Alert>
+          )}
+          <Alert severity="info">
+            Don't have an account?
+            <Link variant="subtitle2" components={RouterLink} to="/register">
+              Get Started
+            </Link>
+          </Alert>
+
+          <FTextField name="email" label="Email Address"></FTextField>
+
+          <FTextField
+            name="password"
+            label="password"
+            type={showPassword ? "text" : "password"}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => setShowPassword(!showPassword)}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          ></FTextField>
+        </Stack>
+      </FormProvider>
+    </Container>
   );
 };
 
