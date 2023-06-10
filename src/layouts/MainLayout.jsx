e099d20d01fe0ac547a68fb399308b6679048a96
bruno <truongtritin.bee@gmail.com>
2023-06-10 20:09:23 +0700

    Coder comm updated

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,13 +3,16 @@ import { Outlet } from "react-router-dom";
 import React from "react";
 import MainHeader from "./MainHeader";
 import MainFooter from "./MainFooter";
+import AlertMsg from "../components/AlertMsg";
 
 ///
 const MainLayout = () => {
   return (
     <Stack sx={{ minHeight: "100vh" }}>
       <MainHeader />
+      <AlertMsg />
       <Outlet />
+
       <Box sx={{ flexGrow: 1 }} />
       <MainFooter />
     </Stack>
